2822a863e3002475af3313d26afa18e3c2c93428
Lucas Steehouwer <l.steehouwer@students.uu.nl>
2019-03-14 18:08:26 +0100

    Moved the footer to the root environment folder. Footer is now generated

--- src/components/root-environment/footer/footer.ts
+++ src/components/root-environment/footer/footer.ts
@@ -1,6 +1,10 @@
 import { Component, Vue } from "vue-property-decorator";
 import WithRender from "./footer.html?style=./footer.scss";
 
+@Component({
+    name: "root-footer"
+})
+@WithRender
 class FooterComponent extends Vue {
 
 }
--- src/components/root-environment/root-environment.ts
+++ src/components/root-environment/root-environment.ts
@@ -3,12 +3,14 @@ import WithRender from "./root-environment.html?style=./root-environment.scss";
 
 import { InitEnvironment } from "src/components/init-environment/init-environment";
 import { ModellingEnvironment } from "src/components/modelling-environment/modelling-environment";
+import { FooterComponent } from "./footer/footer";
 
 @Component({
     name: "root-environment",
     components: {
         "init-environment": InitEnvironment,
         "modelling-environment": ModellingEnvironment,
+        "footer-component": FooterComponent
     }
 })
 @WithRender
@@ -18,7 +20,6 @@ class RootEnvironment extends Vue {
         ModellingEnvironment,
     ];
     current = ModellingEnvironment;
-    // current = DialogManagerComponent;
 }
 
 export { RootEnvironment };
