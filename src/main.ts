bb0da9006d9d1c785e36761825e4e914963a7797
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-03-27 19:03:38 +0100

    Added feedback service. Removed old unneeded services.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-import { Modeller } from "./modeller/modeller"
-import Session from "./services/session-service";
-import Config from "./services/config-service";
-
 import Vue from "vue";
 import RootEnvironment from "src/components/root-environment/root-environment";
 
@@ -9,9 +5,6 @@ import { Store } from "./store/store";
 
 class Main {
     public static main() {
-        // let config = Config.getInstance();
-        // config.baseUrl = "http://localhost:80/~lucas/cora-server/api";
-        
         new Vue({
             store: Store,
             render: (h) => h(RootEnvironment),
