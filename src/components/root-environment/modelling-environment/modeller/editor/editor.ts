4f0a2b2f5ef0d0a64b1f28143d386e9b2ad577de
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-01 16:35:08 +0200

    Moved some dependencies to dev as they are not needed for the actual build.

--- src/components/root-environment/modelling-environment/modeller/editor/editor.ts
+++ src/components/root-environment/modelling-environment/modeller/editor/editor.ts
@@ -1,19 +1,34 @@
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
 import WithRender from "./editor.html?style=./editor.scss";
 
+import { getModule } from "vuex-module-decorators";
+import SettingsModule from "src/store/settings";
+
 import Editor from "src/editor/editor";
+import DrawerOptions, { GridOptions } from "src/editor/drawer/drawer-options";
 
 @WithRender
 @Component({
     name: "editor",
 })
 export default class EditorComponent extends Vue {
-    test: string = "some test text";
-    modeller: Editor | null = null;
+    testProp: boolean = false;
+    editor: Editor | null = null;
 
     mounted() {
         let canvas = <HTMLCanvasElement>document.getElementById("editorCanvas");
-        let modeller = new Editor(canvas);
-        this.modeller = modeller;
+        let editor = new Editor(canvas);
+        this.editor = editor;
+    }
+
+    get settings() {
+        let mod = getModule(SettingsModule, this.$store);
+        console.log(mod);
+        return mod.settings;
+    }
+
+    @Watch('this.settings', {deep: true, immediate: true})
+    onSettingsChange() {
+        console.log("at some point");
     }
 }
--- src/store/settings.ts
+++ src/store/settings.ts
@@ -1,30 +1,51 @@
 import { VuexModule, Module, Mutation } from "vuex-module-decorators";
+import DrawerOptions, { GridOptions } from "src/editor/drawer/drawer-options";
 
 @Module({
     name: "SettingsModule",
     namespaced: true
 })
 export default class SettingsModule extends VuexModule {
-    snapToGrid: boolean = false;
-    drawGrid: boolean = false;
+    settings: DrawerOptions = {
+        minX: -200,
+        maxX: 200,
+        minY: -200,
+        maxY: 200,
+        minZoom: 1,
+        maxZoom: 1 / 10,
+        gridOptions: {
+            drawGrid: true,
+            snapGrid: true,
+            horizontalGridSeperation: 20,
+            verticalGridSeperation: 20,
+        }
+    }
+
+    get snapToGrid() {
+        return this.settings.gridOptions.snapGrid;
+    }
+
+    get drawGrid() {
+        return this.settings.gridOptions.drawGrid;
+    }
 
     @Mutation
     setSnap(snap: boolean) {
-        this.snapToGrid = snap;
+        this.settings.gridOptions.snapGrid = snap;
     }
 
     @Mutation
     setDraw(draw: boolean) {
-        this.drawGrid = draw;
+        this.settings.gridOptions.drawGrid = draw;
     }
 
     @Mutation
     toggleSnap() {
-        this.snapToGrid = !this.snapToGrid;
+        this.settings.gridOptions.snapGrid = !this.settings.gridOptions.snapGrid;
     }
 
     @Mutation
     toggleDraw() {
-        this.drawGrid = !this.drawGrid;
+        this.settings.gridOptions.drawGrid = !this.settings.gridOptions.drawGrid;
     }
 }
