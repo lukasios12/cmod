f4e8752c298ed9f18c33ef13c0dab46410b94a1c
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-09 12:29:11 +0200

    States can be modified using the edit state menu. Needs polishing

--- src/components/root-environment/modelling-environment/modeller/editor/edit-state/edit-state.ts
+++ src/components/root-environment/modelling-environment/modeller/editor/edit-state/edit-state.ts
@@ -1,10 +1,13 @@
-import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+import { Vue, Component, Prop, Emit, Watch } from "vue-property-decorator";
 import WithRender from "./edit-state.html?style=./edit-state.scss";
 
 import { getModule } from "vuex-module-decorators";
 import PetrinetModule from "src/store/petrinet";
 
 import Editor from "src/editor/editor";
+import Marking from "src/editor/system/marking";
+import OmegaTokenCount from "src/editor/system/tokens/omega-token-count";
+import IntegerTokenCount from "src/editor/system/tokens/integer-token-count";
 
 @WithRender
 @Component({
@@ -12,6 +15,30 @@ import Editor from "src/editor/editor";
 })
 export default class EditStateComponent extends Vue {
     @Prop(Editor) editor!: Editor;
+
+    inc(val: string) {
+        if (this.isNum(val)) {
+            let a = Number(val);
+            return (a + 1).toString();
+        }
+        return val;
+    }
+
+    dec(val: string) {
+        if (this.isNum(val)) {
+            let a = Number(val);
+            return (a - 1).toString();
+        }
+        return val;
+    }
+
+    omega() {
+        return new OmegaTokenCount();
+    }
+
+    get petrinet() {
+        return this.editor.petrinet;
+    }
 
     get places() {
         let mod = getModule(PetrinetModule, this.$store);
@@ -21,9 +48,50 @@ export default class EditStateComponent extends Vue {
     }
 
     get state() {
-        let id = this.editor.selectionId;
-        let graph = this.editor.graph;
-        let state = graph.getState(id);
-        return state;
+        if (this.editor) {
+            let id = this.editor.selectionId;
+            let graph = this.editor.graph;
+            let state = graph.getState(id);
+            return state;
+        }
+        return null;
+    }
+
+    get list() {
+        let list = [];
+        if (this.editor && this.state) {
+            let places = this.places;
+            for (let i = 0; i < places.length; i++) {
+                let place = places[i];
+                let tokens = this.state.get(place);
+                let value = tokens.toString();
+                let item = {place: places[i], value: value};
+                list.push(item);
+            }
+        }
+        return list;
+    }
+
+    confirm() {
+        let replacement = new Marking(this.petrinet);
+        let list = this.list;
+        for (let i = 0; i < list.length; i++) {
+            let place = list[i].place;
+            let tokenString = list[i].value;
+            let numerical = /^-?[0-9]+$/.test(tokenString);
+            let tokens;
+            if (this.isNum(tokenString)) {
+                let count = Math.max(0, Number(tokenString));
+                tokens = new IntegerTokenCount(count);
+            } else {
+                tokens = new OmegaTokenCount();
+            }
+            replacement.set(place, tokens);
+        }
+        this.editor.editState(this.editor.selectionId, replacement);
+    }
+
+    protected isNum(val: string) {
+        return /^-?[0-9]*$/.test(val);
     }
 }
