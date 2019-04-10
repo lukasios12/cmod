60876e72a87661f31e9795f7e43ca4cb504c5fe4
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-10 14:32:10 +0200

    Finally the edit state menu works as intended.

--- src/components/root-environment/modelling-environment/modeller/editor/edit-state/edit-state.ts
+++ src/components/root-environment/modelling-environment/modeller/editor/edit-state/edit-state.ts
@@ -16,82 +16,94 @@ import IntegerTokenCount from "src/system/tokens/integer-token-count";
 export default class EditStateComponent extends Vue {
     @Prop(Editor) editor!: Editor;
 
-    inc(val: string) {
-        if (this.isNum(val)) {
-            let a = Number(val);
-            return (a + 1).toString();
+    map = [];
+
+    dec(index: number) {
+        if (index >= 0 && index < this.map.length) {
+            let val = this.map[index].tokens;
+            if (this.isNum(val)) {
+                let num = Math.max(0, Number(val) - 1);
+                this.map[index].tokens = num.toString();
+            }
         }
-        return val;
     }
 
-    dec(val: string) {
-        if (this.isNum(val)) {
-            let a = Number(val);
-            return (a - 1).toString();
+    inc(index: number) {
+        if (index >= 0 && index < this.map.length) {
+            let val = this.map[index].tokens;
+            if (this.isNum(val)) {
+                let num = Math.max(0, Number(val) + 1);
+                this.map[index].tokens = num.toString();
+            }
         }
-        return val;
     }
 
-    omega() {
-        return new OmegaTokenCount();
+    omega(index: number) {
+        if (index >= 0 && this.map.length) {
+            this.map[index].tokens = new OmegaTokenCount().toString();
+        }
     }
 
-    get petrinet() {
-        return this.editor.petrinet;
+    confirm() {
+        if (this.id !== null) {
+            let marking = new Marking(this.editor.petrinet);
+            for(let i = 0; i < this.map.length; i++) {
+                let record = this.map[i];
+                let place  = record.place;
+                let tokens = record.tokens;
+                if (this.isNum(tokens)) {
+                    let num = Math.max(0, Number(tokens));
+                    tokens = new IntegerTokenCount(num);
+                } else {
+                    tokens = new OmegaTokenCount();
+                }
+                marking.set(place, tokens);
+            }
+            this.editor.editState(this.id, marking);
+        }
     }
 
-    get places() {
-        let mod = getModule(PetrinetModule, this.$store);
-        let petrinet = mod.petrinet;
-        let places = petrinet.places;
-        return places.toArray();
+    cancel() {
+        this.setMap();
     }
 
-    get state() {
-        if (this.editor) {
-            let id = this.editor.selectionId;
-            let graph = this.editor.graph;
-            let state = graph.getState(id);
-            return state;
+    get id() {
+        if(this.editor) {
+            return this.editor.selectionId;
         }
         return null;
     }
 
-    get list() {
-        let list = [];
-        if (this.editor && this.state) {
-            let places = this.places;
-            for (let i = 0; i < places.length; i++) {
-                let place = places[i];
-                let tokens = this.state.get(place);
-                let value = tokens.toString();
-                let item = {place: places[i], value: value};
-                list.push(item);
-            }
-        }
-        return list;
+    @Watch('id', {deep: true, immediate: true})
+    onSelectionChange() {
+        this.setMap();
     }
 
-    confirm() {
-        let replacement = new Marking(this.petrinet);
-        let list = this.list;
-        for (let i = 0; i < list.length; i++) {
-            let place = list[i].place;
-            let tokenString = list[i].value;
-            let numerical = /^-?[0-9]+$/.test(tokenString);
-            let tokens;
-            if (this.isNum(tokenString)) {
-                let count = Math.max(0, Number(tokenString));
-                tokens = new IntegerTokenCount(count);
-            } else {
-                tokens = new OmegaTokenCount();
-            }
-            replacement.set(place, tokens);
-        }
-        this.editor.editState(this.editor.selectionId, replacement);
+    get petrinet() {
+        let module = getModule(PetrinetModule, this.$store);
+        return module.petrinet;
     }
 
     protected isNum(val: string) {
         return /^-?[0-9]*$/.test(val);
+    }
+
+    protected createRecord(place: string, tokens: string) {
+        return {place: place, tokens: tokens};
+    }
+
+    protected setMap() {
+        this.map = [];
+        if (this.id !== null) {
+            this.map = [];
+            let graph = this.editor.graph;
+            let state = graph.getState(this.id);
+            let net = this.petrinet;
+            let places = net.places.toArray();
+            for(let i = 0; i < places.length; i++) {
+                let tokenString = state.get(places[i]).toString();
+                this.map.push(this.createRecord(places[i], tokenString));
+            }
+        }
     }
 }
