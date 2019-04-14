5a765bd668239dfa97e94bb64e1cb1f121af8aff
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-14 17:57:20 +0200

    Added remainder function

--- lib/math/math.ts
+++ lib/math/math.ts
@@ -2,4 +2,12 @@ let clamp = function(x: number, min: number, max: number) {
     return Math.max(min, Math.min(x, max));
 }
 
-export { clamp }
+let remainder = function(x: number, mod: number) {
+    let result = x % mod;
+    if (result < 0) {
+        result = mod + result;
+    }
+    return result;
+}
+
+export { clamp, remainder }
