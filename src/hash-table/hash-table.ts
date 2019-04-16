cf153390c0bfe5837accbadab1748e7a16eb4a85
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-17 00:31:54 +0200

    Added hash-table wrapper for the Map data type
    If at some point the Map has to be changed out, then that could be
    simply done by only changing the wrapper.

---
+++ src/hash-table/hash-table.ts
@@ -0,0 +1,56 @@
+export default class HashTable<K, V> {
+    protected table: Map<K, V>;
+
+    public constructor() {
+        this.table = new Map<K, V>();
+    }
+
+    public get(key: K): V {
+        return this.table.get(key);
+    }
+
+    public set(key: K, val: V): void {
+        this.table.set(key, val);
+    }
+
+    public has(key: K): boolean {
+        return this.table.has(key);
+    }
+
+    public remove(key: K): void {
+        return this.table.delete(key);
+    }
+
+    public each(f: (k: K, v: V) => void): void {
+        this.table.forEach((a: V, b: K) => {
+            f(b, a);
+        });
+    }
+
+    public filterKeys(f: (k: K) => boolean): HashTable<K, V> {
+        let result = new HashTable<K, V>();
+        this.each((key: K, value: V) => {
+            if (f(key)) {
+                result.set(key, value);
+            }
+        });
+        return result;
+    }
+
+    public filterValues(f: (v: V) => boolean): HashTable<K, V> {
+        let result = new HashTable<K, V>();
+        this.each((key: K, value: V) => {
+            if (f(value)) {
+                result.set(key, value);
+            }
+        });
+    }
+
+    public isEmpty() {
+        return this.size <= 0;
+    }
+
+    public get size() {
+        return this.table.size;
+    }
+}
