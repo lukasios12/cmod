257f55b589dd2f52e95db4e178edcf72a5a12623
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-22 02:38:52 +0200

    Bootstrap the root environment immediately. No root wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import Vue from "vue";
 import RootEnvironment from "src/components/root-environment/root-environment";
 
 import Store from "./store/store";
 
 class Main {
     public static main() {
-        new Vue({
+        new RootEnvironment({
+            el: "#app",
             store: Store,
-            render: (h) => h(RootEnvironment),
-        }).$mount("#app");
+        });
     }
 }
 
