f03fdaff139d124a8522fbf43a6e93a284763f27
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-04-23 15:58:31 +0200

    Row and col getters for matrix

--- src/matrix/matrix.ts
+++ src/matrix/matrix.ts
@@ -19,21 +19,21 @@ export default class Matrix {
         this.data[row][col] = value;
     }
 
-    public rows(): number {
+    public get rows(): number {
         return this.data.length;
     }
 
-    public cols(): number {
-        return this.rows() > 0 ? this.data[0].length : 0;
+    public get cols(): number {
+        return this.rows > 0 ? this.data[0].length : 0;
     }
 
     public minor(row: number, col: number): Matrix {
-        if(this.rows() <= 1 || this.cols() <= 1) {
+        if(this.rows <= 1 || this.cols <= 1) {
             throw new Error("Could not get minor matrix of 1x1 matrix");
         }
-        let m = new Matrix(this.rows() - 1, this.cols() - 1);
-        for(let r = 0; r < this.rows(); r++) {
-            for (let c = 0; c < this.cols(); c++) {
+        let m = new Matrix(this.rows - 1, this.cols - 1);
+        for(let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
                 if(r == row || c == col) {
                     continue;
                 }
@@ -48,37 +48,37 @@ export default class Matrix {
     }
 
     public cofactor(row: number, col: number): number {
-        if (this.rows() != this.cols()) {
+        if (this.rows != this.cols) {
             throw new Error("Could not compute co-factor for non square matrix");
         }
-        if (this.rows() == 1) {
+        if (this.rows == 1) {
             return Math.pow(-1, row + col) * this.get(0, 0);
         }
         return Math.pow(-1, row + col) * this.minor(row, col).determinant();
     }
 
     public determinant(): number {
-        if(this.rows() != this.cols()) {
+        if(this.rows != this.cols) {
             throw new Error("Could not compute determinant for non square matrix");
         }
-        if(this.rows() == 1) {
+        if(this.rows == 1) {
             return this.get(0, 0);
         }
         let s = 0;
-        for(let i = 0; i < this.rows(); i++) {
+        for(let i = 0; i < this.rows; i++) {
             s += this.get(0,i) * this.cofactor(0, i);
         }
         return s;
     }
 
     public inverse(): Matrix {
-        if(this.rows() != this.cols()) {
+        if(this.rows != this.cols) {
             throw new Error("Could not compute inverse matrix for non-square matrix");
         }
-        let i = new Matrix(this.rows(), this.rows());
+        let i = new Matrix(this.rows, this.rows);
         let d = this.determinant();
-        for(let r = 0; r < this.rows(); r++) {
-            for(let c = 0; c < this.cols(); c++) {
+        for(let r = 0; r < this.rows; r++) {
+            for(let c = 0; c < this.cols; c++) {
                 i.set(r, c, this.minor(r, c).determinant() / d);
             }
         }
@@ -86,9 +86,9 @@ export default class Matrix {
     }
 
     public transpose(): Matrix {
-        let m = new Matrix(this.cols(), this.rows());
-        for(let r = 0; r < this.rows(); r++) {
-            for(let c = 0; c < this.cols(); c++) {
+        let m = new Matrix(this.cols, this.rows);
+        for(let r = 0; r < this.rows; r++) {
+            for(let c = 0; c < this.cols; c++) {
                 m.set(c, r, this.get(r, c));
             }
         }
@@ -96,9 +96,9 @@ export default class Matrix {
     }
 
     public static scale(m: Matrix, f: number): Matrix {
-        let v = new Matrix(m.rows(), m.cols());
-        for(let r = 0; r < m.rows(); r++) {
-            for(let c = 0; c < m.cols(); c++) {
+        let v = new Matrix(m.rows, m.cols);
+        for(let r = 0; r < m.rows; r++) {
+            for(let c = 0; c < m.cols; c++) {
                 v.set(r, c, m.get(r, c) * f);
             }
         }
@@ -106,12 +106,12 @@ export default class Matrix {
     }
 
     public static add(lhs: Matrix, rhs: Matrix): Matrix {
-        if(!(lhs.cols() == rhs.cols() && lhs.rows() == rhs.rows())) {
+        if(!(lhs.cols == rhs.cols && lhs.rows == rhs.rows)) {
             throw new Error("Could not add matrixes of unequal sizes");
         }
-        let result = new Matrix(lhs.rows(), rhs.rows());
-        for(let r = 0; r < lhs.rows(); r++) {
-            for(let c = 0; c < lhs.cols(); c++) {
+        let result = new Matrix(lhs.rows, rhs.rows);
+        for(let r = 0; r < lhs.rows; r++) {
+            for(let c = 0; c < lhs.cols; c++) {
                 result.set(r, c, lhs.get(r,c) + rhs.get(r,c));
             }
         }
@@ -119,14 +119,14 @@ export default class Matrix {
     }
 
     public static mult(lhs: Matrix, rhs: Matrix): Matrix {
-        if(!(lhs.cols() == rhs.rows())) {
+        if(!(lhs.cols == rhs.rows)) {
             throw new Error("Could not multiply matrixes: invalid dimensions");
         }
-        let result = new Matrix(lhs.rows(), rhs.cols());
-        for(let r = 0; r < lhs.rows(); r++) {
-            for(let c = 0; c < rhs.cols(); c++) {
+        let result = new Matrix(lhs.rows, rhs.cols);
+        for(let r = 0; r < lhs.rows; r++) {
+            for(let c = 0; c < rhs.cols; c++) {
                 let k = 0;
-                for(let cc = 0; cc < lhs.rows(); cc++) {
+                for(let cc = 0; cc < lhs.rows; cc++) {
                     k += lhs.get(r, cc) * rhs.get(cc, c);
                 }
                 result.set(r,c,k);
