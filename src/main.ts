fc156a8e0b7a88cd90cec4f3858786ccc5ec0fbb
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-05-03 18:06:23 +0200

    Vue debugging tools disabled when in production mode

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import Vue from "vue";
 import RootEnvironment from "src/components/root-environment/root-environment";
 
 import Store from "./store/store";
 
 class Main {
     public static main() {
+        if (PRODUCTION) {
+            Vue.config.silent = true;
+            Vue.config.devtools = false;
+            Vue.config.performance = false;
+            Vue.config.productionTip = false;
+        }
         new RootEnvironment({
             el: "#app",
             store: Store,
