ef94c5414fb6ae4eed754f9c9697ec8feb1067e9
Lucas Steehouwer <lucas_steehouwer@hotmail.com>
2019-05-06 15:21:52 +0200

    Moved the init environments from the dialogs folder directly into the environment folder

--- src/components/root-environment/init-environment/init-environment.ts
+++ src/components/root-environment/init-environment/init-environment.ts
@@ -1,15 +1,14 @@
 import { Component, Vue } from "vue-property-decorator";
 import WithRender from "./init-environment.html?style=./init-environment.scss";
 
-import WelcomeDialogComponent from "./dialogs/welcome/welcome";
-import UploadDialogComponent from "./dialogs/upload/upload";
+import WelcomeDialogComponent from "./welcome/welcome";
+import UploadDialogComponent from "./upload/upload";
 import LoaderComponent from "src/components/common/loader/loader";
 
 import { getModule } from "vuex-module-decorators";
 import UserModule from "src/store/user";
 import PetrinetModule from "src/store/petrinet";
 import SessionModule from "src/store/session";
-
 
 @WithRender
 @Component({
